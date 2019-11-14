15ee5da3c2ebd05922630bd631e44040c683bef6
GNico <nico.787@hotmail.com>
2019-11-13 23:34:16 -0300

    Agregando Celery, redis y ordenando dependencias con virtualenv

--- backend/tsa/settings.py
+++ backend/tsa/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-
 STATIC_URL = '/static/'
+
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
