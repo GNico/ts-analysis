1349c1e9d3bd290b6b0d15705035dc52dbe14b1d
Juan Martty <jmartty@hotmail.com>
2020-08-09 17:51:30 -0300

    Fixed metrics, WIP, needs more tests

--- backend/prueba/salib/model/test/metric_classifications_builder.py
+++ backend/prueba/salib/model/test/metric_classifications_builder.py
@@ -1,35 +1,4 @@
 import pandas as pd
-from enum import IntEnum
-from functools import total_ordering
-
-
-class AnomalyType(IntEnum):
-    ACTUAL_START = 0,
-    ACTUAL_END = 1,
-    EXPECTED_START = 2,
-    EXPECTED_END = 3
-
-
-@total_ordering
-class TaggedAnomaly:
-
-    def __init__(self, anomaly, anomaly_type, timestamp):
-        self.anomaly = anomaly
-        self.type = anomaly_type
-        self.timestamp = timestamp
-
-    def __eq__(self, other):
-        return ((self.timestamp, self.type) == (other.timestamp, other.type))
-
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __lt__(self, other):
-        return ((self.timestamp, self.type) < (other.timestamp, other.type))
-
-    def __repr__(self):
-        return str(self.type) + "[" + str(self.timestamp) + "]"
-
 
 def build(series, expected_anomalies, actual_anomalies):
 
@@ -41,66 +10,40 @@ def build(series, expected_anomalies, actual_anomalies):
     start = series.start
     end = series.end
 
+    expected_anomalies = sorted(expected_anomalies)
+    actual_anomalies = sorted(actual_anomalies)
+
     assert_no_overlap(expected_anomalies)
     assert_no_overlap(actual_anomalies)
 
-    anomalies_q = sorted_tagged_anomalies(expected_anomalies, actual_anomalies)
-
     last_pos = start
-    current_expected = None
-    current_actual = None
+    last_expected = None
+    last_actual = None
 
-    while anomalies_q:
-        next_anomaly = anomalies_q.pop(0)
-        if next_anomaly.type == AnomalyType.ACTUAL_START:
-            if current_actual is None and current_expected is None:
-                tn.append([last_pos, next_anomaly.timestamp])
-                last_pos = next_anomaly.timestamp
-                current_actual = next_anomaly
-
-        elif next_anomaly.type == AnomalyType.ACTUAL_END:
-            if current_actual is not None and current_expected is not None:
-                tp.append([last_pos, next_anomaly.timestamp])
-                last_pos = next_anomaly.timestamp
-                current_actual = None
-
-        elif next_anomaly.type == AnomalyType.EXPECTED_START:
-            if current_actual is not None and current_expected is None:
-                fn.append([last_pos, next_anomaly.timestamp])
-                last_pos = next_anomaly.timestamp
-                current_expected = next_anomaly
-
-        elif next_anomaly.type == AnomalyType.EXPECTED_END:
-            if current_actual is None and current_expected is not None:
-                fp.append([last_pos, next_anomaly.timestamp])
-                last_pos = next_anomaly.timestamp
-                current_expected = None
-
-    # Edge cases on end
+    # TODO: optimize with bin-search?
+    for i in range(start.second, end.second + 1):
+        ts = pd.Timestamp(i, unit='s')
+        is_actual = timestamp_included(ts, actual_anomalies)
+        is_expected = timestamp_included(ts, expected_anomalies)
+        if is_actual and is_expected:
+            tp.append(ts)
+        elif is_actual and not is_expected:
+            fp.append(ts)
+        elif not is_actual and not is_expected:
+            tn.append(ts)
+        elif not is_actual and is_expected:
+            fn.append(ts)
 
     return (tp, fp, tn, fn)
 
 
-def sorted_tagged_anomalies(expected_anomalies, actual_anomalies):
+def timestamp_included(ts, anomalies):
+    for anom in anomalies:
+        if ts >= anom.start and ts <= anom.end:
+            return True
+    return False
 
-    tagged_expected_anomalies = sum(map(
-        lambda e:
-            [TaggedAnomaly(e, AnomalyType.EXPECTED_START, e.start),
-             TaggedAnomaly(e, AnomalyType.EXPECTED_END, e.end)],
-        expected_anomalies), [])
-
-    tagged_actual_anomalies = sum(map(
-        lambda e:
-            [TaggedAnomaly(e, AnomalyType.ACTUAL_START, e.start),
-             TaggedAnomaly(e, AnomalyType.ACTUAL_END, e.end)],
-        actual_anomalies), [])
-
-    return sorted(list(tagged_actual_anomalies)
-                  + list(tagged_expected_anomalies))
-
-
-def assert_no_overlap(anomalies):
-    sorted_anomalies = sorted(anomalies)
+def assert_no_overlap(sorted_anomalies):
     if len(sorted_anomalies) == 0:
         return
     last_end = sorted_anomalies[0].end
@@ -110,12 +53,5 @@ def assert_no_overlap(anomalies):
         last_end = anomaly.end
 
 
-def interval_from_epochs(expected):
-    return [
-        pd.Timestamp(expected[0], unit='s'),
-        pd.Timestamp(expected[1], unit='s')
-    ]
-
-
-def intervals_from_epochs(expected):
-    return list(map(lambda e: interval_from_epochs(e), expected))
+def epochs_to_timestamp(epochs):
+    return list(map(lambda e: pd.Timestamp(e, unit='s'), epochs))
--- backend/prueba/salib/tests/framework/test/test_testcase.py
+++ backend/prueba/salib/tests/framework/test/test_testcase.py
@@ -11,28 +11,29 @@ from model.test import metric_classifications_builder as cb
 class TestTestCase(unittest.TestCase):
 
     def test_metrics_case1(self):
-        expected_anomalies = [
+        actual_anomalies = [
+            anomaly.Anomaly.from_epoch(1, 1, 1.0),
             anomaly.Anomaly.from_epoch(2, 2, 1.0),
-            anomaly.Anomaly.from_epoch(4, 5, 1.0)
+            anomaly.Anomaly.from_epoch(4, 4, 1.0)
         ]
+
+        analyzer = mock_analyzer.MockAnalyzer(actual_anomalies)
 
         test_series = series.Series.from_array([
             [0, 0],
-            [1, 0],
-            [2, 1],
+            [1, 1],
+            [2, 0],
             [3, 0],
-            [4, 1],
+            [4, 0],
             [5, 1],
             [6, 0]
         ], 1, 's')
 
-        analyzer = mock_analyzer.MockAnalyzer(expected_anomalies)
-
-        actual_anomalies = anomalies_from_mock_series(test_series)
+        expected_anomalies = anomalies_from_mock_series(test_series)
 
         expected_analysis = analysis.Analysis(
             "Test",
-            actual_anomalies,
+            expected_anomalies,
             None,
             None
         )
@@ -41,24 +42,24 @@ class TestTestCase(unittest.TestCase):
         result = test.run()
         anomaly_metrics = result.anomaly_metrics
 
-        expected_tp = cb.intervals_from_epochs([[2, 2], [5, 5]])
-        expected_fp = cb.intervals_from_epochs([[4, 4]])
-        expected_tn = cb.intervals_from_epochs([[0, 0], [3, 3]])
-        expected_fn = cb.intervals_from_epochs([[6, 6]])
+        expected_tp = cb.epochs_to_timestamp([1])
+        expected_fp = cb.epochs_to_timestamp([2, 4])
+        expected_tn = cb.epochs_to_timestamp([0, 3, 6])
+        expected_fn = cb.epochs_to_timestamp([5])
 
         self.assertEqual(expected_tp, anomaly_metrics.tp)
         self.assertEqual(expected_fp, anomaly_metrics.fp)
         self.assertEqual(expected_tn, anomaly_metrics.tn)
         self.assertEqual(expected_fn, anomaly_metrics.fn)
 
-        self.assertEqual(0, anomaly_metrics.tp_count())
-        self.assertEqual(0, anomaly_metrics.tn_count())
-        self.assertEqual(0, anomaly_metrics.fp_count())
-        self.assertEqual(0, anomaly_metrics.fn_count())
+        self.assertEqual(1, anomaly_metrics.tp_count())
+        self.assertEqual(3, anomaly_metrics.tn_count())
+        self.assertEqual(2, anomaly_metrics.fp_count())
+        self.assertEqual(1, anomaly_metrics.fn_count())
 
-        self.assertEqual(1, anomaly_metrics.precision())
-        self.assertEqual(1, anomaly_metrics.recall())
-        self.assertEqual(1, anomaly_metrics.f1())
+        self.assertAlmostEqual(0.33, anomaly_metrics.precision(), 2)
+        self.assertAlmostEqual(0.5, anomaly_metrics.recall(), 2)
+        self.assertAlmostEqual(0.4, anomaly_metrics.f1(), 2)
 
     def test_anomalies_from_mock(self):
         test_series = series.Series.from_array([
