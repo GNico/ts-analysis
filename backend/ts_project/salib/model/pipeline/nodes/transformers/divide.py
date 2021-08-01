101b27e27e2e8b1ab4dc2cf09b9523bfa7ff459d
Juan Martty <jmartty@hotmail.com>
2021-08-01 16:41:50 -0300

    Improve Divide transformer to handle zero div cases

--- backend/ts_project/salib/model/pipeline/nodes/transformers/divide.py
+++ backend/ts_project/salib/model/pipeline/nodes/transformers/divide.py
@@ -2,14 +2,16 @@ import numpy as np
 
 from .binary_math_transformer import BinaryMathTransformer
 from ...params.select import Select, SelectOption
+from ...params.int import Int
 
 class Divide(BinaryMathTransformer):
 
     def __init__(self, id):
         super().__init__(id)
+        self.add_param(Int('zero_div', 'Zero div:', 'Value for division by zero. Empty to skip', 0))
 
     def __str__(self):
-        return 'Divide(' + ','.join(map(str, self.sources)) + ')[' + self.id + ']'
+        return 'Divide(' + str(self.get_zero_div()) + ')[' + self.id + ']'
 
     def display(self):
         return 'Divide'
@@ -17,8 +19,12 @@ class Divide(BinaryMathTransformer):
     def desc(self):
         return 'Divide operator'
 
+    def get_zero_div(self):
+        return self.get_param('zero_div').value
+
     def transform(self, seriess, debug):
         lhs = seriess[0].pdseries
         rhs = seriess[1].pdseries
-        result = (lhs / rhs).replace([np.inf,-np.inf], 0).dropna()
+        zero_div_replacement = np.nan if self.get_zero_div() is None else self.get_zero_div()
+        result = (lhs / rhs).replace([np.inf,-np.inf], zero_div_replacement).dropna()
         return (result, {})
--- backend/ts_project/salib/tests/analyzer/transformers/test_divide.py
+++ backend/ts_project/salib/tests/analyzer/transformers/test_divide.py
@@ -12,7 +12,7 @@ from model.test.testcase import TestCase
 
 class TestDivide(unittest.TestCase):
 
-    def test_divide(self):
+    def test_divide_diff_sizes(self):
         series1 = Series.from_array([
             [1, 1],
             [2, 2],
@@ -24,6 +24,7 @@ class TestDivide(unittest.TestCase):
             [4, 5]]
             , 1)
         factory = NodeFactory.transformer('test', 'Divide')
+        factory.set_param_value('zero_div', 0)
         divide = factory.build()
 
         result, debug_info = divide.transform([series1, series2], False)
@@ -31,3 +32,42 @@ class TestDivide(unittest.TestCase):
         self.assertEqual(list(expected_series.index), list(result.index))
         self.assertEqual(list(expected_series.values), list(result.values))
 
+    def test_divide_discard_zero_div(self):
+        series1 = Series.from_array([
+            [1, 1],
+            [2, 2],
+            [3, 3]]
+            , 1)
+        series2 = Series.from_array([
+            [1, 2],
+            [2, 0],
+            [3, 5]]
+            , 1)
+        factory = NodeFactory.transformer('test', 'Divide')
+        factory.set_param_value('zero_div', None)
+        divide = factory.build()
+
+        result, debug_info = divide.transform([series1, series2], False)
+        expected_series = pd.Series([1/2, 3/5], index=[series1.pdseries.index[0],series1.pdseries.index[2]])
+        self.assertEqual(list(expected_series.index), list(result.index))
+        self.assertEqual(list(expected_series.values), list(result.values))
+
+    def test_divide_discard_fill_zero_div(self):
+        series1 = Series.from_array([
+            [1, 1],
+            [2, 2],
+            [3, 3]]
+            , 1)
+        series2 = Series.from_array([
+            [1, 2],
+            [2, 0],
+            [3, 5]]
+            , 1)
+        factory = NodeFactory.transformer('test', 'Divide')
+        factory.set_param_value('zero_div', 0)
+        divide = factory.build()
+
+        result, debug_info = divide.transform([series1, series2], False)
+        expected_series = pd.Series([1/2, 0.0, 3/5], index=series1.pdseries.index)
+        self.assertEqual(list(expected_series.index), list(result.index))
+        self.assertEqual(list(expected_series.values), list(result.values))
