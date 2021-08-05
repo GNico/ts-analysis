fd7b80143189dabdfd2e8141d107bf457c1f7d4b
Juan Martty <jmartty@hotmail.com>
2021-08-05 20:27:35 -0300

    Add more tests to Derivative

--- backend/ts_project/salib/tests/analyzer/transformers/test_derivative.py
+++ backend/ts_project/salib/tests/analyzer/transformers/test_derivative.py
@@ -2,6 +2,7 @@ import unittest
 
 from model.analyzer import Analyzer
 from model.anomaly import Anomaly
+from model.series import Series
 from model.analysis import Analysis
 from model.pipeline.pipeline import Pipeline
 from model.pipeline.node_factory import NodeFactory
@@ -21,6 +22,42 @@ class TestDerivative(unittest.TestCase):
 
         result, debug_info = diff.transform([series1], False)
 
-        expected_series = [1.0] * 9
-        self.assertEqual(expected_series, list(result.values))
+        expected_series = Series.from_array([
+            [1, 1],
+            [2, 1],
+            [3, 1],
+            [4, 1],
+            [5, 1],
+            [6, 1],
+            [7, 1],
+            [8, 1],
+            [9, 1],
+            ], interval=1)
 
+        self.assertEqual(list(expected_series.pdseries.index), list(result.index))
+        self.assertEqual(list(expected_series.pdseries.values), list(result.values))
+
+
+    def test_base_derivative_lag2(self):
+        series1 = TestSeriesBuilder.linear(10, 0, 1).build()
+
+        factory = NodeFactory.transformer('test', 'Derivative')
+        factory.set_param_value('lag', '2')
+        factory.set_param_value('metric', 'sub')
+        diff = factory.build()
+
+        result, debug_info = diff.transform([series1], False)
+
+        expected_series = Series.from_array([
+            [2, 2],
+            [3, 2],
+            [4, 2],
+            [5, 2],
+            [6, 2],
+            [7, 2],
+            [8, 2],
+            [9, 2],
+            ], interval=1)
+
+        self.assertEqual(list(expected_series.pdseries.index), list(result.index))
+        self.assertEqual(list(expected_series.pdseries.values), list(result.values))
